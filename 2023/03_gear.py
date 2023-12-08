f97af103155bf976241281325843ea94b7a294bb
Jason <chiyoung.jang@mail.mcgill.ca>
2023-12-08 14:14:18 -0500

    gear part 1 answer too high

--- 2023/03_gear.py
+++ 2023/03_gear.py
@@ -8,16 +8,43 @@ def parse(data):
 
 
 def part1(data):
+  result = []
   for row in range(len(data)-1):
     for col in range(len(data[row])):
       if ord('0') <= ord(data[row][col]) <= ord('9') or ord(data[row][col]) == ord('.'):
         pass
       else:
+        current_t = data[row][col]
         # print(data[row-1][col-1], data[row-1][col], data[row-1][col+1], data[row][col-1], data[row][col+1], data[row+1][col-1], data[row+1][col], data[row+1][col+1])
         # represents which sections are touching the symbol
-        if ord('0') <= ord(data[row-1][col-1]) <= ord('9'):
-          pass
-          # solve for a way to figure out the full digit that is touching the symbol
+        for r in range(-1, 2):
+          for c in range(-1, 2):
+            current_s = data[row+r][col+c]
+            if ord('0') <= ord(data[row+r][col+c]) <= ord('9'):
+              i = 1
+              j = 1
+              num = data[row+r][col+c]
+              while True:
+                if col+c-i > 0 and ord('0') <= ord(data[row+r][col+c-i]) <= ord('9'):
+                  num = data[row+r][col+c-i] + num
+                  i += 1
+                elif col+c+j < len(data) and ord('0') <= ord(data[row+r][col+c+j]) <= ord('9'):
+                  num = num + data[row+r][col+c+j]
+                  j += 1
+                else:
+                  result.append(int(num))
+                  break
+  for ind, number in enumerate(result):
+    if ind > 0:
+      if number == result[ind-1]:
+        result.pop(ind)
+  return print(sum(result))
+
+
+
+# while loop checking if neighbours are numbers then use a temp variable to hold current digit? two pointers?
+# solve for a way to figure out the full digit that is touching the symbol
 # print(parse(puzzle.input_data)) test
 # parse("467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598..")
-part1(parse("467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598.."))
+# part1(parse("467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598.."))
+part1(parse(puzzle.input_data))
